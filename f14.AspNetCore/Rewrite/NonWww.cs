f79988012bd40c747e7a886eda3a1bab70796476
f14shm4n <uraborg@gmail.com>
2018-01-14 19:11:40 +0300

    Transfered Rewrite classes from old project.

---
+++ f14.AspNetCore/Rewrite/NonWww.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Rewrite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace f14.AspNetCore.Engine.Rewrite
+{
+    /// <summary>
+    /// Provides the redirect rule which redirect "www.sample.com" to "sample.com".
+    /// </summary>
+    public class NonWww : IRule
+    {
+        private bool _permanent = false;
+
+        public NonWww(bool premanent)
+        {
+            _permanent = premanent;
+        }
+
+        public void ApplyRule(RewriteContext context)
+        {
+            var request = context.HttpContext.Request;
+            var host = request.Host;
+            if (host.Host.StartsWith("www."))
+            {
+                var nonWwwHost = new HostString(host.Host.Substring(4), host.Port ?? 80);
+                var sb = new StringBuilder()
+                    .Append("http://")
+                    .Append(nonWwwHost)
+                    .Append(request.PathBase)
+                    .Append(request.Path)
+                    .Append(request.QueryString);
+                context.HttpContext.Response.Redirect(sb.ToString(), _permanent);
+                context.Result = RuleResult.EndResponse;
+            }
+        }
+    }
+}
