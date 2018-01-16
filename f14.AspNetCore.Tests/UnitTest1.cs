9bb6154451aa0c5fb64515b73fb42ef8f74e0a7f
f14shm4n <uraborg@gmail.com>
2018-01-16 15:41:08 +0300

    Added test proj.

---
+++ f14.AspNetCore.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+using f14.AspNetCore.Identity;
+using System;
+using Xunit;
+
+namespace f14.AspNetCore.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
