1a27e542a41a1eb9b4cad1de17a75f840990b71f
f14shm4n <uraborg@gmail.com>
2018-01-22 21:30:13 +0300

    Transfered mails services.

---
+++ f14.AspNetCore.Mail/EmailSender.cs
@@ -0,0 +1,43 @@
+﻿using f14.AspNetCore.Mail;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace f14.AspNetCore.Mail
+{
+    public class EmailSender : IEmailSender
+    {
+        private string _server;
+        private int _port;
+        private bool _useSsl;
+        private string _login;
+        private string _password;
+        private string _fromName;
+        private string _fromEmail;
+
+        public EmailSender(string server, int port, bool useSsl, string login, string password, string fromName, string fromEmail)
+        {
+            ExHelper.NotNullString(() => server);
+            ExHelper.NotNullString(() => login);
+            ExHelper.NotNullString(() => password);
+            ExHelper.NotNullString(() => fromEmail);            
+
+            _server = server;
+            _port = port;
+            _useSsl = useSsl;
+            _login = login;
+            _password = password;
+            _fromName = fromName;
+            _fromEmail = fromEmail;
+        }
+
+        public Task SendAsync(string toEmail, string subject, string message)
+        {
+            ExHelper.NotNullString(() => toEmail);
+            ExHelper.NotNullString(() => subject);
+            ExHelper.NotNullString(() => message);
+            return MessageService.SendEmailAsync(_server, _port, _useSsl, _login, _password, _fromName, _fromEmail, "", toEmail, subject, message);
+        }
+    }
+}
