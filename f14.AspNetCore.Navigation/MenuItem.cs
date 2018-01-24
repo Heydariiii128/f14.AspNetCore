a965ed2160ede6d71fb414cba60b5b975a8ce134
f14shm4n <uraborg@gmail.com>
2018-01-24 17:12:19 +0300

    Added project for navigation components.

---
+++ f14.AspNetCore.Navigation/MenuItem.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace f14.AspNetCore.Navigation
+{
+    /// <summary>
+    /// Represents as base navigation menu item.
+    /// </summary>
+    public class MenuItem
+    {
+        /// <summary>
+        /// The header of the object.
+        /// </summary>
+        public string Header { get; set; }
+        /// <summary>
+        /// The target url.
+        /// </summary>
+        public string Url { get; set; }
+        /// <summary>
+        /// The element icon.
+        /// </summary>
+        public string Icon { get; set; }
+        /// <summary>
+        /// The nested items.
+        /// </summary>
+        public ICollection<MenuItem> Items { get; set; } = new List<MenuItem>();
+        /// <summary>
+        /// Adds nested item.
+        /// </summary>
+        /// <param name="item">An item to add.</param>
+        /// <returns>The current object.</returns>
+        public MenuItem Add(MenuItem item)
+        {
+            ExHelper.NotNull(() => item);
+
+            Items.Add(item);
+            return this;
+        }
+    }
+}
---
+++ f14.AspNetCore.Navigation/NavigationMenu.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace f14.AspNetCore.Navigation
+{
+    /// <summary>
+    /// Represents the navigation menu.
+    /// </summary>
+    public class NavigationMenu
+    {
+        /// <summary>
+        /// Navigation items.
+        /// </summary>
+        public List<MenuItem> Items { get; set; } = new List<MenuItem>();
+        /// <summary>
+        /// Adds navigation item to the menu.
+        /// </summary>
+        /// <param name="item">An menu item.</param>
+        /// <returns>The current object.</returns>
+        public NavigationMenu Add(MenuItem item)
+        {
+            Items.Add(item);
+            return this;
+        }
+    }
+}
