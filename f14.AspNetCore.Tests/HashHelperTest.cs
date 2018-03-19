ab9168ee7a67d977afbe5044c17546a09e108860
f14shm4n <uraborg@gmail.com>
2018-03-19 21:48:08 +0300

    Added hash helper test.

---
+++ f14.AspNetCore.Tests/HashHelperTest.cs
@@ -0,0 +1,33 @@
+﻿using f14.AspNetCore.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace f14.AspNetCore.Tests
+{
+    public class HashHelperTest
+    {
+        private ITestOutputHelper Log { get; }
+
+        public HashHelperTest(ITestOutputHelper log)
+        {
+            Log = log;
+        }
+
+        [Theory]
+        [InlineData("Sample data for hash.", "Sample hash salt.")]
+        [InlineData("Тестовые данные для хэша.", "Тестовая соль для хэша.")]
+        public void ComputeAndValidate(string data, string salt)
+        {
+            var hash = HashHelper.Compute(data, Encoding.Unicode.GetBytes(salt));
+
+            Log.WriteLine($"Text: {data}\nHash: {hash}");
+
+            Assert.NotEqual(data, hash);
+
+            Assert.True(HashHelper.Validate(hash, data, Encoding.Unicode.GetBytes(salt)));
+        }
+    }
+}
