4aaa472bc525f200806fa562ac0a27cf062fdb38
f14shm4n <uraborg@gmail.com>
2018-11-25 19:44:40 +0300

    Update packages.

---
+++ f14.AspNetCore.Mvc/MvcHelper.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace f14.AspNetCore.Mvc
+{
+    /// <summary>
+    /// Provides helpful methods for MVC framework.
+    /// </summary>
+    public static class MvcHelper
+    {
+        /// <summary>
+        /// Gets name of the type. Also, extracts controller name without 'Controller' word.
+        /// </summary>
+        /// <typeparam name="T">Type of object.</typeparam>
+        /// <returns>The name of the object.</returns>
+        public static string GetName<T>() => GetName(typeof(T));
+        /// <summary>
+        /// Gets name of the type. Also, extracts controller name without 'Controller' word.
+        /// </summary>
+        /// <param name="type">The sepcified type.</param>
+        /// <returns>The name of the object.</returns>
+        public static string GetName(Type type)
+        {
+            ExHelper.NotNull(() => type);
+            string name = type.Name;
+            if (name.EndsWith("Controller"))
+                return name.Replace("Controller", "");
+            else
+                return name;
+        }
+    }
+}
